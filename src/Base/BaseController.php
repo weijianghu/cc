8e6efa4e7263e029ae5b29c179c8c2f1ab3e35a7
jianghu <76112703@qq.com>
2022-05-19 13:33:56 +0800

    project init

---
+++ src/Base/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2022/5/19
+ * Time: 9:34
+ */
+namespace Jiklika\Cc\Base;
+
+class BaseController
+{
+    public function __construct()
+    {
+        echo "project init...";
+    }
+}
---
+++ src/common.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2022/5/19
+ * Time: 9:37
+ */
+function logs(){
+    echo "thi is a log writer";
+}
